a4c062f38353f0b8854bd41212cdfe5d2d1e5cfe
github username <github email address>
2019-10-08 17:00:09 +0100

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
+  resp = Rack::Response.new
+
+get '/' do
+resp.write "Hello, World!"
+end
 
 end
